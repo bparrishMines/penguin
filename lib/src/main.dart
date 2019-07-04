b7f6b40da291174e5077a0ad99bf0d01f9059c5a
Maurice Parrish <bmparr@google.com>
2019-07-04 13:52:11 -0700

    Finish create portion

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -5,6 +5,9 @@
 import 'dart:io';
 
 import 'package:args/args.dart';
+import 'package:path/path.dart' as path;
+
+import 'penguin_option.dart';
 
 void main(List<String> args) {
   final ArgParser parser = new ArgParser();
@@ -23,7 +26,9 @@ void main(List<String> args) {
         option.name,
         abbr: option.abbr,
         help: option.help,
+        defaultsTo: option.defaultsTo,
         callback: option.callback,
+        valueHelp: option.valueHelp,
       );
     }
   }
@@ -36,117 +41,34 @@ void main(List<String> args) {
     exit(64);
   }
 
-  //final ProcessResult result = Process.runSync('pwd', <String>[]);
+  if (results[PenguinOption.usage.name]) {
+    print(parser.usage);
+    exit(0);
+  } else if (results.rest.isNotEmpty) {
+    print(parser.usage);
+    exit(64);
+  }
 
-  //final Directory directory = Directory('./');
+  final Directory directory = Directory(path.join(
+    Directory.current.path,
+    results[PenguinOption.directory.name],
+  ));
+
+  _runFlutterCreate(directory, results[PenguinOption.projectName.name]);
 }
 
-class PenguinOption implements Option {
-  const PenguinOption._({
-    this.abbr,
-    this.allowed,
-    this.allowedHelp,
-    this.callback,
-    this.defaultsTo,
-    this.help,
-    this.hide,
-    this.isFlag,
-    this.isMultiple,
-    this.isSingle,
-    this.name,
-    this.negatable,
-    this.splitCommas,
-    this.type,
-    this.valueHelp,
-  })  : assert(name != null),
-        assert(isFlag != null);
-
-  static PenguinOption usage = PenguinOption._(
-    name: 'help',
-    abbr: 'h',
-    help: 'Print usage.',
-    negatable: false,
-    isFlag: true,
+void _runFlutterCreate(Directory directory, String projectName) {
+  final ProcessResult result = Process.runSync(
+    'flutter',
+    <String>[
+      'create',
+      '-t',
+      'plugin',
+      '--project-name',
+      projectName,
+      directory.path,
+    ],
   );
-
-  static PenguinOption projectName = PenguinOption._(
-    name: 'project-name',
-    abbr: 'n',
-    valueHelp: 'name',
-    help:
-        'The project name for this new Flutter project. This must be a valid dart package name.',
-    defaultsTo: 'new_plugin',
-    isFlag: false,
-  );
-
-  static PenguinOption directory = PenguinOption._(
-    name: 'directory',
-    abbr: 'd',
-    help: 'Output path for plugin.',
-    defaultsTo: './',
-    valueHelp: 'path',
-    isFlag: false,
-  );
-
-  static List<PenguinOption> values = <PenguinOption>[
-    usage,
-    projectName,
-    directory,
-  ];
-
-  @override
-  final String abbr;
-
-  @override
-  @deprecated
-  final String abbreviation = null;
-
-  @override
-  final List<String> allowed;
-
-  @override
-  final Map<String, String> allowedHelp;
-
-  @override
-  final Function callback;
-
-  @override
-  @deprecated
-  final dynamic defaultValue = null;
-
-  @override
-  final dynamic defaultsTo;
-
-  @override
-  void getOrDefault(dynamic value) => value ?? defaultsTo;
-
-  @override
-  final String help;
-
-  @override
-  final bool hide;
-
-  @override
-  final bool isFlag;
-
-  @override
-  final bool isMultiple;
-
-  @override
-  final bool isSingle;
-
-  @override
-  final String name;
-
-  @override
-  final bool negatable;
-
-  @override
-  final bool splitCommas;
-
-  @override
-  final OptionType type;
-
-  @override
-  final String valueHelp;
+  print(result.stdout);
+  print(result.stderr);
 }
