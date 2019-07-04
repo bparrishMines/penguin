f3ad42b7eb952ca2704fa839d4eef9ec01b63eae
Maurice Parrish <bmparr@google.com>
2019-07-03 18:51:02 -0700

    Add arguments parser

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -2,8 +2,39 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:args/command_runner.dart';
+import 'package:args/args.dart';
 
 void main(List<String> args) {
-  
+  final ArgParser parser = new ArgParser();
+  parser
+    ..addFlag(
+      'help',
+      abbr: 'h',
+      help: 'Print usage.',
+      negatable: false,
+      callback: (bool help) {
+        if (help) print(parser.usage);
+      },
+    )
+    ..addOption(
+      'project-name',
+      abbr: 'n',
+      valueHelp: 'name',
+      help:
+          'The project name for this new Flutter project. This must be a valid dart package name.',
+      defaultsTo: 'new_plugin',
+    )
+    ..addOption(
+      'directory',
+      abbr: 'd',
+      help: 'Output path for plugin.',
+      defaultsTo: './',
+      valueHelp: 'path',
+    );
+
+  try {
+    final ArgResults results = parser.parse(args);
+  } on FormatException catch (exception) {
+    print(exception.message);
+  }
 }
