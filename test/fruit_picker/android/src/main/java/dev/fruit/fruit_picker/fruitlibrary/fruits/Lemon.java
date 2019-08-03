2ae60a8bfbbe976c77bc3fe87ef82110740e0e3c
Maurice Parrish <bmparr@google.com>
2019-08-03 14:39:33 -0700

    Add lemon

---
+++ test/fruit_picker/android/src/main/java/dev/fruit/fruit_picker/fruitlibrary/fruits/Lemon.java
@@ -0,0 +1,7 @@
+package dev.fruit.fruit_picker.fruitlibrary.fruits;
+
+public class Lemon {
+  public Lemon(String color, Double sourness) {}
+
+  public void makeLemonade(Boolean addSugar, Boolean addIce) {}
+}
