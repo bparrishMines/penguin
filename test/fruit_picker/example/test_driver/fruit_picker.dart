bbbd97f67efef57297d8a7c2b95a23944fccd568
Maurice Parrish <bmparr@google.com>
2019-08-03 14:32:58 -0700

    add multi param constructor test, multi param method test and try to fix gitignore

--- test/fruit_picker/example/test_driver/fruit_picker.dart
+++ test/fruit_picker/example/test_driver/fruit_picker.dart
@@ -7,6 +7,7 @@ import 'package:fruit_picker/apple.dart';
 import 'package:fruit_picker/banana.dart';
 import 'package:fruit_picker/orange.dart';
 import 'package:fruit_picker/strawberry.dart';
+import 'package:fruit_picker/lemon.dart';
 
 void main() {
   final Completer<String> completer = Completer<String>();
@@ -80,6 +81,18 @@ void main() {
 
       test('seeds', () {
         expect(Strawberry.averageNumberOfSeeds, completion(50));
+      });
+    });
+
+    group('$Lemon', () {
+      Lemon lemon;
+
+      setUp(() {
+        lemon = Lemon('Yellow', 42.0);
+      });
+
+      test('makeLemonade', () {
+        expect(lemon.makeLemonade(true, false), completes);
       });
     });
   });
