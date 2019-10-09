88568c6e5f7370cb8cae1d3a1332641be3622986
Maurice Parrish <bmparr@google.com>
2019-10-08 19:49:46 -0700

    Driver tests pass but im tired of working on them

--- penguin_generator/test/penguin_generator_test.dart
+++ penguin_generator/test/penguin_generator_test.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:path/path.dart' as p;
@@ -5,61 +6,65 @@ import 'package:test/test.dart';
 
 void main() {
   group('penguin_generator', () {
-    test('test_plugin', () async {
-      bool success;
-
+    test('test_plugin test_driver tests', () async {
       print('Running flutter create...');
-      success = _printProcessResult(Process.runSync('flutter', <String>[
-        'create',
-        '--project-name=test_plugin',
-        '--no-with-driver-test',
-        '--template=plugin',
-        '--androidx',
-        '--android-language=java',
-        '--ios-language=objc',
-        p.join('test', 'test_plugin'),
-      ]));
-      if (!success) exit(64);
+      expect(
+        await _printProcess(await Process.start('flutter', <String>[
+          'create',
+          '--project-name=test_plugin',
+          '--no-with-driver-test',
+          '--template=plugin',
+          '--androidx',
+          '--android-language=java',
+          '--ios-language=objc',
+          p.join('test', 'test_plugin'),
+        ])),
+        isTrue,
+      );
 
       print('Running build_runner...');
-      success = _printProcessResult(Process.runSync(
-        'flutter',
-        <String>[
-          'packages',
-          'pub',
-          'run',
-          'build_runner',
-          'build',
-        ],
-        workingDirectory: p.join('test', 'test_plugin'),
-      ));
-      if (!success) exit(64);
+      expect(
+        await _printProcess(await Process.start(
+          'flutter',
+          <String>[
+            'packages',
+            'pub',
+            'run',
+            'build_runner',
+            'build',
+          ],
+          workingDirectory: p.join('test', 'test_plugin'),
+        )),
+        isTrue,
+      );
 
       print('Moving platform specific files...');
       await Future.wait<File>(<Future<File>>[_moveAndroidGeneratedCode()]);
 
       print('Running test_driver...');
-      success = _printProcessResult(Process.runSync(
+      final Process driverTestProcess = await Process.start(
         'flutter',
         <String>['drive', p.join('test_driver', 'test_plugin.dart')],
         workingDirectory: p.join('test', 'test_plugin', 'example'),
-      ));
+      );
 
-      expect(success, isTrue);
-    });
+      bool testsPassed = false;
+      driverTestProcess.stdout.transform(utf8.decoder).listen((String data) {
+        if (data.contains('All tests passed!')) testsPassed = true;
+        stdout.write(data);
+      });
+      stderr.addStream(driverTestProcess.stderr);
+
+      expect(await driverTestProcess.exitCode == 0 && testsPassed, isTrue);
+    }, timeout: Timeout(Duration(minutes: 2)));
   });
 }
 
 // Returns whether script runs successfully
-bool _printProcessResult(ProcessResult result) {
-  final bool success = result.exitCode == 0;
-
-  if (!success) {
-    print(result.stdout);
-    print(result.stderr);
-  }
-
-  return success;
+Future<bool> _printProcess(Process process) async {
+  stdout.addStream(process.stdout);
+  stderr.addStream(process.stderr);
+  return (await process.exitCode) == 0;
 }
 
 Future<File> _moveAndroidGeneratedCode() =>
