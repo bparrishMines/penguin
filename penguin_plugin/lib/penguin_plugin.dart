80421170bed7881904dcd532f050dbdca4a5d7e9
Maurice Parrish <bmparr@google.com>
2019-12-20 18:38:54 -0800

    Original commit of penguin plugin

---
+++ penguin_plugin/lib/penguin_plugin.dart
@@ -0,0 +1,94 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+abstract class Wrapper {
+  const Wrapper(this.uniqueId, {this.onCreateView});
+
+  final String uniqueId;
+  final List<MethodCall> Function($Context context) onCreateView;
+
+  String get platformClassName;
+  List<MethodCall> onMethodCall(MethodCall call);
+
+  MethodCall allocate() {
+    return MethodCall(
+      '$platformClassName#allocate',
+      <String, String>{r'$uniqueId': uniqueId},
+    );
+  }
+
+  MethodCall deallocate() {
+    return MethodCall(
+      '$platformClassName#deallocate',
+      <String, String>{r'$uniqueId': uniqueId},
+    );
+  }
+}
+
+Future<T> invoke<T>(
+  MethodChannel channel,
+  MethodCall call, [
+  Iterable<MethodCall> following = const <MethodCall>[],
+]) {
+  final Completer<T> completer = Completer<T>();
+
+  invokeAll(
+    channel,
+    <MethodCall>[call, ...following].where((MethodCall call) => call != null),
+  ).then(
+    (List<dynamic> results) => completer.complete(results.last),
+  );
+
+  return completer.future;
+}
+
+Future<List<T>> invokeList<T>(
+  MethodChannel channel,
+  MethodCall call, [
+  Iterable<MethodCall> following = const <MethodCall>[],
+]) {
+  final Completer<List<T>> completer = Completer<List<T>>();
+
+  invokeAll(
+    channel,
+    <MethodCall>[call, ...following].where((MethodCall call) => call != null),
+  ).then(
+    (List<dynamic> results) => completer.complete(results.last.cast<T>()),
+  );
+
+  return completer.future;
+}
+
+Future<Map<S, T>> invokeMap<S, T>(
+  MethodChannel channel,
+  MethodCall call, [
+  Iterable<MethodCall> following = const <MethodCall>[],
+]) {
+  final Completer<Map<S, T>> completer = Completer<Map<S, T>>();
+
+  invokeAll(
+    channel,
+    <MethodCall>[call, ...following].where((MethodCall call) => call != null),
+  ).then(
+    (List<dynamic> results) => completer.complete(results.last.cast<S, T>()),
+  );
+
+  return completer.future;
+}
+
+Future<List<dynamic>> invokeAll(
+  MethodChannel channel,
+  Iterable<MethodCall> methodCalls,
+) {
+  final List<Map<String, dynamic>> serializedCalls = methodCalls
+      .map<Map<String, dynamic>>(
+        (MethodCall methodCall) => <String, dynamic>{
+          'method': methodCall.method,
+          'arguments': methodCall.arguments,
+        },
+      )
+      .toList();
+
+  return channel.invokeListMethod<dynamic>('MultiInvoke', serializedCalls);
+}
