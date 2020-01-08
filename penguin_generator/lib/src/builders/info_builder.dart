dab38be05cf9364c9b74ef60ff6f724d99a79748
Maurice Parrish <bmparr@google.com>
2020-01-07 17:44:53 -0800

    Technical debt and prob fixed a bug

--- penguin_generator/lib/src/builders/info_builder.dart
+++ penguin_generator/lib/src/builders/info_builder.dart
@@ -10,94 +10,11 @@ import 'package:source_gen/source_gen.dart';
 import '../info.dart';
 import 'annotation_utils.dart';
 
-//class PenguinBuilderBuildStep {
-//  PenguinBuilderBuildStep._(this._buildStep) {}
-//
-//  static final String _fileHeader = r'''
-//// GENERATED CODE - DO NOT MODIFY BY HAND
-//
-//// **************************************************************************
-//// PenguinGenerator
-//// **************************************************************************
-//''';
-//
-//  final BuildStep _buildStep;
-//
-//  AssetId get inputId => _buildStep.inputId;
-//
-//  Future<void> writeToAsset(AssetId assetId, String contents) {
-//    return _buildStep.writeAsString(assetId, contents);
-//  }
-//
-//  Future<void> writeToLib(String filename, String content) {
-//    return _buildStep.writeAsString(
-//      AssetId(_buildStep.inputId.package, p.join('lib', filename)),
-//      _fileHeader + content,
-//    );
-//  }
-//}
-//
-//abstract class PenguinBuilder {
-//  Iterable<String> get filenames;
-//  // Platform annotation for the builder (e.g. AndroidPlatform)
-//  Iterable<Type> get platformTypes;
-//
-//  Future<void> build(
-//    List<ClassInfo> libraryClasses,
-//    List<ClassInfo> importedClasses,
-//    PenguinBuilderBuildStep buildStep,
-//  );
-//
-//  // For passing methods over MethodChannel
-//  MethodChannelType getChannelType(TypeInfo info) {
-//    if (info.isMap ||
-//        info.isList &&
-//            info.typeArguments.every(
-//              (_) => getChannelType(_) == MethodChannelType.supported,
-//            )) {
-//      return MethodChannelType.supported;
-//    } else if (info.isStruct) {
-//      return MethodChannelType.struct;
-//    } else if (info.isNativeInt32 || info.isNativeInt64) {
-//      // We must check for primitive before supported because a type will be both.
-//      return MethodChannelType.primitive;
-//    } else if (info.isDynamic ||
-//        info.isObject ||
-//        info.isString ||
-//        info.isNum ||
-//        info.isInt ||
-//        info.isDouble ||
-//        info.isBool) {
-//      return MethodChannelType.supported;
-//    } else if (info.isVoid) {
-//      return MethodChannelType.$void;
-//    } else if (info.isWrapper) {
-//      return MethodChannelType.wrapper;
-//    } else if (info.isTypeParameter) {
-//      return MethodChannelType.typeParameter;
-//    }
-//
-//    throw ArgumentError.value(
-//      info.toString(),
-//      'info',
-//      'Can\'t find $MethodChannelType for info',
-//    );
-//  }
-//
-//  String removeBounds(String value) {
-//    final RegExp genericBrackets = RegExp('<.*>');
-//    return value.replaceAll(genericBrackets, '');
-//  }
-//}
-
 class InfoBuilder extends Builder {
   static const String infoExtension = '.info';
 
   static const String libraryClassesKey = 'libraryClasses';
   static const String importedClassesKey = 'importedClasses';
-
-  final Set<Element> _allElements = <Element>{};
-  final Set<ClassInfo> _extraClasses = <ClassInfo>{};
 
   @override
   FutureOr<void> build(BuildStep buildStep) async {
@@ -106,32 +23,48 @@ class InfoBuilder extends Builder {
 
     final LibraryReader reader = LibraryReader(await buildStep.inputLibrary);
 
-    final List<ClassInfo> libraryClasses = reader
-        .annotatedWith(Annotation.$class)
-        .map<ClassInfo>((AnnotatedElement element) {
-      _allElements.add(element.element);
-      return _toClassInfo(
-        element.element,
-        AnnotationUtils.classFromConstantReader(element.annotation),
-      );
-    }).toList();
+    final _InfoBuilderHelper builderHelper = _InfoBuilderHelper(
+      reader.annotatedWith(Annotation.$class).map<ClassElement>(
+          (AnnotatedElement annotatedElement) => annotatedElement.element),
+    );
 
-    if (libraryClasses.isEmpty) return Future<void>.value();
+    if (builderHelper.libraryClasses.isEmpty) return Future<void>.value();
 
     buildStep.writeAsString(
       buildStep.inputId.changeExtension(infoExtension),
       jsonEncode(<String, List<ClassInfo>>{
-        libraryClassesKey: libraryClasses,
-        importedClassesKey:
-            _extraClasses.difference(libraryClasses.toSet()).toList(),
+        libraryClassesKey: builderHelper.libraryClasses,
+        importedClassesKey: builderHelper.importedClasses,
       }),
     );
   }
 
-  ClassInfo _toClassInfo(ClassElement classElement, Class classAnnotation) =>
-      ClassInfo(
+  @override
+  Map<String, List<String>> get buildExtensions => <String, List<String>>{
+        '.dart': <String>[infoExtension],
+      };
+}
+
+class _InfoBuilderHelper {
+  _InfoBuilderHelper(this.libraryClassElements) {
+    _allReferencedClassElements.addAll(libraryClassElements);
+    libraryClasses.addAll(libraryClassElements.map<ClassInfo>(
+        (ClassElement classElement) => _toClassInfo(classElement)));
+  }
+
+  final Iterable<ClassElement> libraryClassElements;
+  final Set<ClassElement> _allReferencedClassElements = <ClassElement>{};
+
+  final List<ClassInfo> libraryClasses = <ClassInfo>[];
+  final List<ClassInfo> importedClasses = <ClassInfo>[];
+
+  ClassInfo _toClassInfo(ClassElement classElement) => ClassInfo(
         name: classElement.name,
-        aClass: classAnnotation,
+        aClass: AnnotationUtils.classFromConstantReader(
+          ConstantReader(
+            Annotation.$class.firstAnnotationOfExact(classElement),
+          ),
+        ),
         constructors: classElement.constructors
             .where((ConstructorElement constructorElement) =>
                 Annotation.constructor.hasAnnotationOfExact(constructorElement))
@@ -147,7 +80,7 @@ class InfoBuilder extends Builder {
                 parameters: constructorElement.parameters.map<ParameterInfo>(
                   (ParameterElement parameterElement) => ParameterInfo(
                     name: parameterElement.name,
-                    type: _toTypeInfo(
+                    type: toTypeInfo(
                       parameterElement.type,
                       parameterElement,
                     ),
@@ -173,7 +106,7 @@ class InfoBuilder extends Builder {
                 parameters: methodElement.parameters.map<ParameterInfo>(
                   (ParameterElement parameterElement) => ParameterInfo(
                     name: parameterElement.name,
-                    type: _toTypeInfo(
+                    type: toTypeInfo(
                       parameterElement.type,
                       parameterElement,
                     ),
@@ -182,12 +115,12 @@ class InfoBuilder extends Builder {
                 name: methodElement.name,
                 returnType: methodElement.returnType.isDartAsyncFuture ||
                         methodElement.returnType.isDartAsyncFutureOr
-                    ? _toTypeInfo(
+                    ? toTypeInfo(
                         (methodElement.returnType as ParameterizedType)
                             .typeArguments[0],
                         methodElement,
                       )
-                    : _toTypeInfo(
+                    : toTypeInfo(
                         methodElement.returnType,
                         methodElement,
                       ),
@@ -215,15 +148,15 @@ class InfoBuilder extends Builder {
                 (FieldElement fieldElement) => FieldInfo(
                   isMutable: fieldElement.isPublic && !fieldElement.isFinal,
                   isStatic: fieldElement.isStatic,
-                  name: _getFieldName(fieldElement),
+                  name: getRealFieldName(fieldElement),
                   type: fieldElement.type.isDartAsyncFuture ||
                           fieldElement.type.isDartAsyncFutureOr
-                      ? _toTypeInfo(
+                      ? toTypeInfo(
                           (fieldElement.type as ParameterizedType)
                               .typeArguments[0],
                           fieldElement,
                         )
-                      : _toTypeInfo(
+                      : toTypeInfo(
                           fieldElement.type,
                           fieldElement,
                         ),
@@ -257,15 +190,15 @@ class InfoBuilder extends Builder {
                 (PropertyAccessorElement accessorElement) => FieldInfo(
                   isMutable: accessorElement.isSetter,
                   isStatic: accessorElement.isStatic,
-                  name: _getFieldName(accessorElement),
+                  name: getRealFieldName(accessorElement),
                   type: accessorElement.variable.type.isDartAsyncFuture ||
                           accessorElement.variable.type.isDartAsyncFutureOr
-                      ? _toTypeInfo(
+                      ? toTypeInfo(
                           (accessorElement.variable.type as ParameterizedType)
                               .typeArguments[0],
                           accessorElement,
                         )
-                      : _toTypeInfo(
+                      : toTypeInfo(
                           accessorElement.variable.type,
                           accessorElement,
                         ),
@@ -279,32 +212,28 @@ class InfoBuilder extends Builder {
         ],
         typeParameters: classElement.typeParameters.map<TypeInfo>(
           (TypeParameterElement typeParameterElement) =>
-              _toTypeInfo(typeParameterElement.type, typeParameterElement),
+              toTypeInfo(typeParameterElement.type, typeParameterElement),
         ),
       );
 
-  TypeInfo _toTypeInfo(DartType type, Element element) {
-    final bool isWrapper =
-        !type.isVoid && Annotation.$class.hasAnnotationOfExact(type.element);
+  TypeInfo toTypeInfo(DartType type, Element element) {
+    final ClassElement typeClassElement =
+        type.element is ClassElement ? type.element : null;
 
-    if (isWrapper &&
-        type.element is ClassElement &&
-        _allElements.add(type.element)) {
-      _extraClasses.add(_toClassInfo(
-        type.element,
-        AnnotationUtils.classFromConstantReader(
-          ConstantReader(
-            Annotation.$class.firstAnnotationOfExact(type.element),
-          ),
-        ),
-      ));
+    final bool isWrapper = !type.isVoid &&
+        typeClassElement != null &&
+        Annotation.$class.hasAnnotationOfExact(typeClassElement);
+
+    if (isWrapper && !_allReferencedClassElements.contains(typeClassElement)) {
+      _allReferencedClassElements.add(typeClassElement);
+      importedClasses.add(_toClassInfo(typeClassElement));
     }
 
     return TypeInfo(
       name: type.toString(),
       typeArguments: type is ParameterizedType && type.typeArguments.isNotEmpty
           ? type.typeArguments.map<TypeInfo>(
-              (DartType type) => _toTypeInfo(
+              (DartType type) => toTypeInfo(
                 type,
                 element,
               ),
@@ -328,19 +257,19 @@ class InfoBuilder extends Builder {
       isVoid: type.isVoid,
       isWrapper: isWrapper,
       isStruct:
-          isWrapper && type.element is ClassElement && _isStruct(type.element),
+          isWrapper && typeClassElement != null && isStruct(typeClassElement),
       isTypeParameter: type is TypeParameterType,
       isNativeInt32: Annotation.int32.hasAnnotationOfExact(element),
       isNativeInt64: Annotation.int64.hasAnnotationOfExact(element),
     );
   }
 
-  bool _isStruct(ClassElement classElement) {
-    final Platform platform = _getPlatform(classElement);
+  bool isStruct(ClassElement classElement) {
+    final Platform platform = getPlatform(classElement);
     return platform is IosPlatform && platform.type.isStruct;
   }
 
-  Platform _getPlatform(ClassElement classElement) {
+  Platform getPlatform(ClassElement classElement) {
     final ConstantReader classReader = ConstantReader(
       Annotation.$class.firstAnnotationOfExact(classElement),
     );
@@ -352,93 +281,14 @@ class InfoBuilder extends Builder {
     return annotation.platform;
   }
 
-  String _getFieldName(Element element) {
+  String getRealFieldName(Element element) {
     final Field field = AnnotationUtils.fieldFromConstantReader(
       ConstantReader(
         Annotation.field.firstAnnotationOfExact(element),
       ),
     );
 
-    // Accessors include = in their names
+    // Accessors include '=' in their names
     return field.nameOverride ?? element.name.replaceAll('=', '');
   }
-
-  @override
-  Map<String, List<String>> get buildExtensions => <String, List<String>>{
-        '.dart': <String>[infoExtension],
-      };
 }
-
-//class PlatformWriteBuilder extends Builder {
-//  PlatformWriteBuilder(this.platformBuilders);
-//
-//  static final Glob _allInfoFiles =
-//      Glob('lib/**${ReadInfoBuilder._infoExtension}');
-//
-//  final List<PenguinBuilder> platformBuilders;
-//
-//  @override
-//  FutureOr<void> build(BuildStep buildStep) async {
-//    final Set<ClassInfo> libraryClasses = <ClassInfo>{};
-//    Set<ClassInfo> importedClasses = <ClassInfo>{};
-//
-//    await for (AssetId input in buildStep.findAssets(_allInfoFiles)) {
-//      final String json = await buildStep.readAsString(input);
-//      libraryClasses.addAll(_getLibraryClasses(json));
-//      importedClasses.addAll(_getImportedClasses(json));
-//    }
-//
-//    if (libraryClasses.isEmpty) return;
-//
-//    importedClasses = importedClasses.difference(libraryClasses);
-//
-//    await Future.wait<void>(
-//      platformBuilders.map<Future<void>>(
-//        (PenguinBuilder builder) => builder.build(
-//          _classesForPlatforms(libraryClasses, builder.platformTypes),
-//          _classesForPlatforms(importedClasses, builder.platformTypes),
-//          PenguinBuilderBuildStep._(buildStep),
-//        ),
-//      ),
-//    );
-//  }
-//
-//  @override
-//  Map<String, List<String>> get buildExtensions => <String, List<String>>{
-//        r'$lib$': platformBuilders
-//            .expand<String>((PenguinBuilder builder) => builder.filenames)
-//            .toList(),
-//      };
-//}
-
-//class DartWriteBuilder extends Builder {
-//  DartWriteBuilder(this.platformBuilders);
-//
-//  final List<PenguinBuilder> platformBuilders;
-//
-//  @override
-//  FutureOr<void> build(BuildStep buildStep) async {
-//    final String json = await buildStep.readAsString(buildStep.inputId);
-//    final Iterable<ClassInfo> libraryClasses = _getLibraryClasses(json);
-//    final Iterable<ClassInfo> importedClasses = _getImportedClasses(json);
-//
-//    if (libraryClasses.isEmpty) return;
-//
-//    await Future.wait<void>(
-//      platformBuilders.map<Future<void>>(
-//        (PenguinBuilder builder) => builder.build(
-//          _classesForPlatforms(libraryClasses, builder.platformTypes),
-//          _classesForPlatforms(importedClasses, builder.platformTypes),
-//          PenguinBuilderBuildStep._(buildStep),
-//        ),
-//      ),
-//    );
-//  }
-//
-//  @override
-//  Map<String, List<String>> get buildExtensions => <String, List<String>>{
-//        ReadInfoBuilder._infoExtension: platformBuilders
-//            .expand<String>((PenguinBuilder builder) => builder.filenames)
-//            .toList(),
-//      };
-//}
