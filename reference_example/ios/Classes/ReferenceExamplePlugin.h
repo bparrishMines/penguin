e96a38288570712896431e980c9b05761009b560
Maurice Parrish <bparr2450@gmail.com>
2020-07-10 13:58:27 -0700

    Initial commit of reference_example

---
+++ reference_example/ios/Classes/ReferenceExamplePlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface ReferenceExamplePlugin : NSObject<FlutterPlugin>
+@end
---
+++ reference_example/ios/Classes/ReferenceExamplePlugin.m
@@ -0,0 +1,20 @@
+#import "ReferenceExamplePlugin.h"
+
+@implementation ReferenceExamplePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"reference_example"
+            binaryMessenger:[registrar messenger]];
+  ReferenceExamplePlugin* instance = [[ReferenceExamplePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
