3cd502cbfe7586893d7d0d84cd396c5fef8db328
Maurice Parrish <bparr2450@gmail.com>
2021-02-17 16:47:48 -0800

    fix tests

--- packages/ios_avfoundation/example/lib/main.dart
+++ packages/ios_avfoundation/example/lib/main.dart
@@ -1,45 +1,22 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
+// @dart=2.9
 
-import 'package:flutter/services.dart';
-import 'package:ios_avfoundation/ios_avfoundation.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await IosAvfoundation.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -49,8 +26,8 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: const Center(
+          child: Text('Hello, World!'),
         ),
       ),
     );
