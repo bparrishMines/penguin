016a8aa8539cd8fd114e77236c602ba394ca3b1f
Maurice Parrish <bparr2450@gmail.com>
2021-05-30 22:03:51 -0700

    fix example

--- packages/reference_example/example/lib/main.dart
+++ packages/reference_example/example/lib/main.dart
@@ -36,8 +36,8 @@ class _MyAppState extends State<MyApp> {
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () async {
-            final ClassTemplate classTemplate = ClassTemplate(23);
-            final String result = await classTemplate.methodTemplate('Hello,');
+            final $$class_name$$ classTemplate = $$class_name$$(23);
+            final String result = await classTemplate.$__method_name__('Hello,');
             setState(() {
               _text = result;
             });
