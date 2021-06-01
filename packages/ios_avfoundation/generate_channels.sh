0644e88ca7c864e1ffeedf4811958ee9eb5b9827
Maurice Parrish <bparr2450@gmail.com>
2021-05-31 21:11:14 -0700

    update dart code

---
+++ packages/ios_avfoundation/generate_channels.sh
@@ -0,0 +1,2 @@
+flutter pub run reference_generator --branch master --dart-out lib/src/avfoundation.g.dart --dart-imports 'dart:typed_data' lib/src/avfoundation.dart
+#flutter pub run reference_generator --no-build --branch master --java-package github.bparrishMines.penguin.penguin_android_camera --java-out android/src/main/java/github/bparrishMines/penguin/penguin_android_camera/CameraChannelLibrary.java lib/src/camera.dart
