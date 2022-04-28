d0d13f6a98d921a2d244c2c6b7ccc389f071b7f7
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-04-28 15:24:16 -0700

    try fix extra new lines

--- packages/code_template_processor/bin/token.dart
+++ packages/code_template_processor/bin/token.dart
@@ -277,6 +277,11 @@ Token? tryParseToken(
     templateQueue.removeFirst();
   }
 
+  // Removes newlines that follow a token.
+  if (templateQueue.isNotEmpty && templateQueue.first == '\n'){
+    templateQueue.removeFirst();
+  }
+
   final String tokenString = tokenStringBuffer.toString();
   if (tokenString.isEmpty) {
     return EndToken();
