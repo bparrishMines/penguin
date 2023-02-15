e69387f6843c0a4495b6f5a5d484b10fd10d35d7
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2023-02-15 15:03:53 -0500

    dart create start

---
+++ packages/gen_api_impls/gen_api_impls/bin/gen_api_impls.dart
@@ -0,0 +1,5 @@
+import 'package:gen_api_impls/gen_api_impls.dart' as gen_api_impls;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gen_api_impls.calculate()}!');
+}
---
+++ packages/gen_api_impls/gen_api_impls/test/gen_api_impls_test.dart
@@ -0,0 +1,8 @@
+import 'package:gen_api_impls/gen_api_impls.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
